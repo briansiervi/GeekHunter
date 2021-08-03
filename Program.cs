86047fe0cb3d1c49ae8db85dae5d83a838464766
Brian Mazini Siervi <brianmazini@gmail.com>
2021-08-03 11:56:33 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+public class Test
+{
+    public static string processInput(string input)
+    {
+        return input;
+    }
+
+    public static void Main()
+    {
+        foreach (string line in File.ReadLines(@"input.txt"))
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
